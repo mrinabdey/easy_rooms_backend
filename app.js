2b26614938804fe2176f0a5f94dba4cb512018d2
52punk <52punk2017@gmail.com>
2022-05-20 14:10:23 +0530

    Updated mongodb url

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const authRoutes = require('./routes/auth');
-const featuresRoutes = require('./routes/features');
-const userRoutes = require('./routes/user');
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const featuresRoutes = require("./routes/features");
+const userRoutes = require("./routes/user");
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/auth', authRoutes);
-app.use('/features', featuresRoutes);
-app.use('/user', userRoutes);
+app.use("/auth", authRoutes);
+app.use("/features", featuresRoutes);
+app.use("/user", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  "mongodb+srv://easyrooms:Fiber-0009@cluster0.p4nvm.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 const db = mongoose.connection;
 
-db.on('error', () => {
-    console.log('Connection Error!');
+db.on("error", () => {
+  console.log("Connection Error!");
 });
 
-db.once('open', () => {
-    app.listen(port, () => {
-        console.log(`Server is running at port ${port}`);
-    });
+db.once("open", () => {
+  app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+  });
 });
